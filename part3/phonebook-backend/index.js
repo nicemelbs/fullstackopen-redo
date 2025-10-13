5cabab9a5f909a72dfe407650f1efc4d35dd145f
Melvin San Jose <sanjose.melvin@gmail.com>
2025-10-13 15:08:33 +0800

    Complete exercises 3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -23,6 +24,23 @@ let persons = [
     number: '39-23-6423122',
   },
 ]
+// morgan(':method :url :status :res[content-length] - :response-time ms')
+
+const requestLogger = morgan((tokens, request, response) => {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'),
+    '-',
+    tokens['response-time'](request, response),
+    'ms',
+    JSON.stringify(request.body),
+  ].join(' ')
+})
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/info', (request, response) => {
   response.send(
@@ -50,7 +68,6 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.use(express.json())
 const getRandomId = (max) => {
   return String(Math.floor(Math.random() * max))
 }
