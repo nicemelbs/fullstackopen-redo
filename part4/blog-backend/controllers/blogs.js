ed19f9991ac32ca6b817f4ae9e0114d1eb5b1e2a
Melvin San Jose <sanjose.melvin@gmail.com>
2025-10-19 04:29:50 +0800

    Complete exercises 4.8 - 4.17

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -2,23 +2,28 @@ const blogsRouter = require('express').Router()
 
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-    .catch((error) => next(error))
+blogsRouter.get('/', async (request, response, next) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch((error) => next(error))
+  if (blog) response.json(blog)
+  else response.status(404).end()
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  const id = request.params.id
+  await Blog.findOneAndDelete({ _id: id })
+  response.status(204).end()
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/blog-backend/tests/list_helper.test.js
+++ part4/blog-backend/tests/list_helper.test.js
@@ -1,118 +1,66 @@
-const { test, describe } = require('node:test')
+const { test, describe, beforeEach, after } = require('node:test')
 const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+const app = require('../app')
+const api = supertest(app)
+const helper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
 
-  const result = listHelper.dummy(blogs)
+  const result = helper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
 
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0,
-  },
-]
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0,
-  },
-]
-
 describe('total likes', () => {
   test('of empty list is zero', () => {
-    assert.strictEqual(listHelper.totalLikes([]), 0)
+    assert.strictEqual(helper.totalLikes([]), 0)
   })
   test('when list has only one blog equals the likes of that', () => {
-    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+    assert.strictEqual(helper.totalLikes([helper.initialBlogs[0]]), 7)
   })
   test('of a bigger list is calculated right', () => {
-    assert.strictEqual(listHelper.totalLikes(blogs), 36)
+    assert.strictEqual(helper.totalLikes(helper.initialBlogs), 36)
   })
 })
 
 describe('favoriteBlog', () => {
   test('empty list returns null', () => {
-    assert.deepStrictEqual(listHelper.favoriteBlog([]), null)
+    assert.deepStrictEqual(helper.favoriteBlog([]), null)
   })
 
   test('if only one blog, return the same blog', () => {
     assert.deepStrictEqual(
-      listHelper.favoriteBlog(listWithOneBlog),
-      listWithOneBlog[0]
+      helper.favoriteBlog([helper.initialBlogs[0]]),
+      helper.initialBlogs[0]
     )
   })
 
   test('return the blog with most likes', () => {
-    assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[2])
+    assert.deepStrictEqual(
+      helper.favoriteBlog(helper.initialBlogs),
+      helper.initialBlogs[2]
+    )
   })
 })
 
 describe('mostBlogs', () => {
   test('empty list returns null', () => {
-    assert.deepStrictEqual(listHelper.mostBlogs([]), null)
+    assert.deepStrictEqual(helper.mostBlogs([]), null)
   })
 
   test('if only one blog, return the author and blog:1', () => {
-    assert.deepStrictEqual(listHelper.mostBlogs(listWithOneBlog), {
-      author: 'Edsger W. Dijkstra',
+    assert.deepStrictEqual(helper.mostBlogs([helper.initialBlogs[0]]), {
+      author: 'Michael Chan',
       blogs: 1,
     })
   })
 
   test('return the proper value', () => {
-    assert.deepStrictEqual(listHelper.mostBlogs(blogs), {
+    assert.deepStrictEqual(helper.mostBlogs(helper.initialBlogs), {
       author: 'Robert C. Martin',
       blogs: 3,
     })
@@ -121,19 +69,133 @@ describe('mostBlogs', () => {
 
 describe('mostLikes', () => {
   test('empty list returns null', () => {
-    assert.deepStrictEqual(listHelper.mostLikes([]), null)
+    assert.deepStrictEqual(helper.mostLikes([]), null)
   })
 
   test('if only one blog, return author and likes of that blog', () => {
-    assert.deepStrictEqual(listHelper.mostLikes(listWithOneBlog), {
-      author: 'Edsger W. Dijkstra',
-      likes: 5,
+    assert.deepStrictEqual(helper.mostLikes([helper.initialBlogs[0]]), {
+      author: 'Michael Chan',
+      likes: 7,
     })
   })
   test('return proper value', () => {
-    assert.deepStrictEqual(listHelper.mostLikes(blogs), {
+    assert.deepStrictEqual(helper.mostLikes(helper.initialBlogs), {
       author: 'Edsger W. Dijkstra',
       likes: 17,
     })
+  })
+})
+
+describe.only('blogs', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+  test('HTTP GET request works properly', async () => {
+    const initialBlogs = helper.initialBlogs
+
+    const fetchedBlogs = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(initialBlogs.length, fetchedBlogs.body.length)
+  })
+
+  test('can fetch one blog by id', async () => {
+    const blogs = await helper.blogsInDb()
+
+    const fetchedBlog = await api
+      .get(`/api/blogs/${blogs[0].id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(blogs[0], fetchedBlog.body)
+  })
+
+  test('get request to non-existent id returns 404', async () => {
+    const nonExistentId = await helper.nonExistingId()
+    await api.get(`/api/blogs/${nonExistentId}`).expect(404)
+  })
+
+  test('identifier of blogs object is id instead of _id', async () => {
+    const id = '5a422bc61b54a676234d17fc'
+
+    const result = await api.get(`/api/blogs/${id}`)
+
+    assert(Object.prototype.hasOwnProperty.call(result.body, 'id'))
+    assert(!Object.prototype.hasOwnProperty.call(result.body, '_id'))
+  })
+
+  test('post request successfully creates a blog', async () => {
+    const newBlog = {
+      title: 'Ang Diary ni Larry',
+      author: 'Ultimate Ube',
+      url: 'https://youtube.com',
+      likes: 20,
+    }
+
+    const blogsBefore = await helper.blogsInDb()
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAfter = await helper.blogsInDb()
+
+    assert.strictEqual(blogsBefore.length + 1, blogsAfter.length)
+    const titles = blogsAfter.map((b) => b.title)
+    assert(titles.includes(newBlog.title))
+  })
+
+  test('if likes property is missing from the request, default to 0', async () => {
+    const blogWithoutLikes = {
+      title: 'Nobody Likes Me',
+      author: 'The Unlikeable',
+      url: 'https://no.likes.here',
+    }
+
+    const result = await api
+      .post('/api/blogs')
+      .send(blogWithoutLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(result.body.likes, 0)
+  })
+
+  test('if title or url properties are missing, return HTTP 400', async () => {
+    const blogWithoutTitle = {
+      author: 'The Unlikeable',
+      url: 'https://no.likes.here',
+      likes: 2,
+    }
+
+    await api.post('/api/blogs').send(blogWithoutTitle).expect(400)
+
+    const blogWithoutUrl = {
+      title: 'This One Has No URL',
+      author: 'The Unlikeable',
+      likes: 2,
+    }
+
+    await api.post('/api/blogs').send(blogWithoutUrl).expect(400)
+  })
+
+  test('a blog can be deleted', async () => {
+    const id = helper.initialBlogs[0]._id
+    const blogsAtStart = await helper.blogsInDb()
+    await api.delete(`/api/blogs/${id}`).expect(204)
+    const blogsAtEnd = await helper.blogsInDb()
+
+    await api.get(`/api/blogs/${id}`).expect(404)
+
+    assert.strictEqual(blogsAtStart.length - 1, blogsAtEnd.length)
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
   })
 })
