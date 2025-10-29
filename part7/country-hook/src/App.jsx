5acee5907f78658a78ab5604871e5f4cd76d5790
Melvin San Jose <sanjose.melvin@gmail.com>
2025-10-30 06:34:56 +0800

    Complete exercise 7.7

---
+++ part7/country-hook/src/App.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name'
+
+  useEffect(() => {
+    const fetchFromApi = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/${name}`)
+
+        const data = {
+          name: response.data.name.common,
+          capital: response.data.capital ? response.data.capital : 'no capital',
+          population: response.data.population,
+          flag: response.data.flags.png,
+        }
+
+        const countryObject = { found: true, data }
+        setCountry(countryObject)
+      } catch (error) {
+        console.error(
+          `No country found matching that name: ${name.toUpperCase()}`
+        )
+        setCountry({ found: false })
+      }
+    }
+
+    if (name === '') setCountry(null)
+    else fetchFromApi()
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return <div>not found...</div>
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
